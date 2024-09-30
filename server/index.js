1530fd940ebdf4f4aa46aa4939045d3cf4a2ce84
Muhammad Hassan <engineermirzahassan@gmail.com>
2024-09-30 19:35:33 +0500

    Setup Complete

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // backend/src/index.js
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.SERVERPORT || 5000;
 
 app.use(express.json());
 
