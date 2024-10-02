ff49fa90ee62395e23a70785a2d96b725ff95c64
Muhammad Hassan <engineermirzahassan@gmail.com>
2024-10-02 12:54:45 +0500

    Fixed Stations List Bug

--- server/services/trainServices/index.js
+++ server/services/trainServices/index.js
@@ -1,11 +1,15 @@
 const axios = require('axios');
 
 const getAllTrains = async (req, res) => {
-    const response = await axios.get('https://api.pakraillive.com/api/Train/GetTrainStations');
-    console.log('response', response)
-    return(
-        res.json("Request Complete")
-    )
+    try {
+        const response = await axios.get('https://api.pakraillive.com/api/Train/GetTrainStations');
+        console.log('res', res)
+        res.status(200).json(response.data);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Something went wrong while fetching train stations' });
+    }
 }
 
 module.exports = { getAllTrains }
