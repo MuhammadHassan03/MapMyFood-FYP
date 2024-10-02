c5e097037e3375ba6272673123c74de1f0e15491
Muhammad Hassan <engineermirzahassan@gmail.com>
2024-10-02 16:34:49 +0500

    Fix Building

--- packages/Shared/config/tamagui.config.tsx
+++ packages/Shared/config/tamagui.config.tsx
@@ -2,8 +2,6 @@ import { createTamagui } from '@tamagui/core';
 import { config } from '@tamagui/config/v3';
 
 
-// Create Tamagui config
 const tamaguiConfig = createTamagui(config);
 
-// Export the configuration
 export default tamaguiConfig;
