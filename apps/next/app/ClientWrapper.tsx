0448555be16798295135dd2eaafd8b856510110b
Muhammad Hassan <engineermirzahassan@gmail.com>
2024-10-02 16:38:54 +0500

    fixed

--- apps/next/app/ClientWrapper.tsx
+++ apps/next/app/ClientWrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TamaguiProvider, createTamagui } from '@tamagui/core';
+import { TamaguiProvider } from '@tamagui/core';
 // import { config } from '@tamagui/config/v3';
 import tamaguiConfig from 'Shared/config/tamagui.config';
 
