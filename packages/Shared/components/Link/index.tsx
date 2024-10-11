a87b9c25364872a3e4c0d6116be0e241f94f62f2
Muhammad Hassan <engineermirzahassan@gmail.com>
2024-10-12 03:45:06 +0500

    Vercel Build ailed

--- packages/Shared/components/Link/index.tsx
+++ packages/Shared/components/Link/index.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import { Text } from 'tamagui';
+import Link, { LinkProps as NextLinkProps } from 'next/link';
 
-let CustomLink;
+interface CustomLinkProps extends NextLinkProps {
+  children: React.ReactNode;
+}
+
+let CustomLink: React.FC<CustomLinkProps>;
+
 if (typeof window !== 'undefined') {
   const NextLink = require('next/link').default;
+
   CustomLink = ({ href, children, ...props }) => (
-    <NextLink href={href} {...props}>
+    <NextLink href={href} {...(props as NextLinkProps)}>
       <Text>{children}</Text>
     </NextLink>
   );
 } else {
-  CustomLink = ({ href, children }) => (
+  CustomLink = ({ children }) => (
     <Text>{children}</Text>
   );
 }
 
-const Link = (props) => {
+interface LinkComponentProps {
+  href: string;
+  children: React.ReactNode;
+  [key: string]: any;
+}
+
+const LinkComponent: React.FC<LinkComponentProps> = (props) => {
   return <CustomLink {...props} />;
 };
 
-export default Link;
+export default LinkComponent;
