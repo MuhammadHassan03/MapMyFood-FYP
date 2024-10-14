a27f03b5e8434acae69f0fc130323595be17e46d
Muhammad Hassan <engineermirzahassan@gmail.com>
2024-10-14 19:03:34 +0500

    DOTENV FIXXED

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -17,7 +18,6 @@ app.use(cors(corsOptions));
 
 app.options('*', cors(corsOptions));
 
-require('dotenv').config();
 app.use(express.json());
 
 app.get('/', (req, res) => {
