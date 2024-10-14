40bd3d89e11ad7388982cd244a97d1eba93d99a2
Muhammad Hassan <engineermirzahassan@gmail.com>
2024-10-14 18:33:53 +0500

    CORS ISSUE

--- server/index.js
+++ server/index.js
@@ -7,30 +7,31 @@ const { trainRoutes } = require('./routes/trainRoutes');
 const { restaurantRouter } = require('./routes/restaurantRoutes');
 
 const corsOptions = {
-  origin: '*https://map-my-food.vercel.app', // Allows all origins
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allow common methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Add other headers if needed
-  credentials: true // Include credentials if necessary
+  origin: 'https://map-my-food.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); 
 
 app.options('*', cors(corsOptions));
-require('dotenv').config()
+
+require('dotenv').config();
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello from server'
-  })
-})
+    message: 'Hello from server',
+  });
+});
 
 connectDB();
 app.use('/api/auth', authRouter);
 app.use('/api/train', trainRoutes);
-app.use('/api/restaurant', restaurantRouter)
+app.use('/api/restaurant', restaurantRouter);
 
-const PORT = 9000
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
