bb7740caa7dcb4f5f7c124eabc9c40ff90f991a2
Muhammad Hassan <engineermirzahassan@gmail.com>
2024-10-14 18:20:26 +0500

    API Error Fixed

--- packages/Shared/constants/API.tsx
+++ packages/Shared/constants/API.tsx
@@ -1,5 +1,5 @@
 const APIURL = () => {
-    const API = 'https://mapmyfood-backend-gp63g0ta7-muhammad-hassans-projects-49e3cfc3.vercel.app/?vercelToolbarCode=_t-LGCq8WZ2zNYl/api';
+    const API = 'https://mapmyfood-backend.vercel.app//api';
     return { API };}
 
 export { APIURL }
