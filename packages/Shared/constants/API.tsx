657758a66530bb06cc51c889c3aa0fbc17ea8661
Muhammad Hassan <engineermirzahassan@gmail.com>
2024-10-14 18:11:13 +0500

    API URL Added

--- packages/Shared/constants/API.tsx
+++ packages/Shared/constants/API.tsx
@@ -1,5 +1,5 @@
 const APIURL = () => {
-    const API = process.env.SERVERURL || 'https://mapmyfood-backend-gp63g0ta7-muhammad-hassans-projects-49e3cfc3.vercel.app/?vercelToolbarCode=_t-LGCq8WZ2zNYl/api';
+    const API = 'https://mapmyfood-backend-gp63g0ta7-muhammad-hassans-projects-49e3cfc3.vercel.app/?vercelToolbarCode=_t-LGCq8WZ2zNYl/api';
     return { API };}
 
 export { APIURL }
